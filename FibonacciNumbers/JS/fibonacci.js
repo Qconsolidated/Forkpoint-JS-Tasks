33f29b769ecc1ae44183b6c58ed32cb1f62030f1
Simeon Nedelchev <simonnedelchev@gmail.com>
2019-11-15 11:47:29 +0200

    Added Fibonacci numbers task

---
+++ FibonacciNumbers/JS/fibonacci.js
@@ -0,0 +1,16 @@
+
+function fibonacci(num)
+{
+	if (num <=1) return 1;
+
+	return fibonacci(num - 1) + fibonacci(num-2);	
+	 
+}
+
+for (var i = 0; i < 9; i++) 
+{
+	document.write(fibonacci(i) + ", ");
+}
+document.write(fibonacci(9));
+
+
