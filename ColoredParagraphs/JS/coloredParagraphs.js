57b62ad7d66fcada3ed2a1af9f785e25eef3d908
Simeon Nedelchev <simonnedelchev@gmail.com>
2019-11-29 03:31:20 +0200

    added potential fix to paragraph task

--- ColoredParagraphs/JS/coloredParagraphs.js
+++ ColoredParagraphs/JS/coloredParagraphs.js
@@ -1,29 +1,20 @@
-var test = document.createElement('style');
-test.innerHTML = '#content p:nth-child(2n) {' + 'background-color: green;' + '}'
-					+ '#content p:nth-child(2n+1){ ' + 'color:red;' + '}';
 
-var ref = document.querySelector('script');
-
-ref.parentNode.insertBefore(test, ref);
-
-/* Make this work instead
 function colorParag()
 {
-	var even = 	document.querySelectorAll("#content div:nth-child(2n)");
-	var odd = document.querySelectorAll("#content div:nth-child(odd)");
-	var evenLength = even.length;
-	var oddLength = odd.length;
+	var even = document.querySelectorAll('#content > p:nth-child(even)');
+	var odd = document.querySelectorAll('#content > p:nth-child(odd)');
+	
 
 	for (var i = 0; i < even.length; i++)
 	{
-	 	even[i].style.backgroundColor = "green";
+	 	even[i].style.backgroundColor = 'green';
 	}
 
 	for (var j = 0; j < odd.length; j++)
 	{
-	 	odd[j].style.color = "green";
+	 	odd[j].style.color = 'red';
 	}
 }
 
 colorParag();
-*/
+
